c1917c3f2d3b8987557a6b2604906fd3de891dea
Geraldo Castro <victormatheuscastro@gmail.com>
2019-06-01 08:30:43 -0300

    Comment/remove marshmallow serializer

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flask_migrate import Migrate
-from flask_marshmallow import Marshmallow
+# from flask_marshmallow import Marshmallow
 from flask_cors import CORS 
 from flask_jwt_extended import JWTManager
 from flask_mongoengine import MongoEngine
@@ -12,7 +12,7 @@ CORS(app)
 app.config.from_pyfile('config.py')
 db = MongoEngine(app)
 # db = SQLAlchemy(app)
-ma = Marshmallow(app)
+# ma = Marshmallow(app)
 api = Api(app)
 migrate = Migrate(app, db)
 jwt = JWTManager(app)
