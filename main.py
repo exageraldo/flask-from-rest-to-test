f35e096fdbc334c09cf3269fdd3259e432c5e7d8
Geraldo Castro <victormatheuscastro@gmail.com>
2019-06-01 08:32:19 -0300

    Comment/remove migrate lib

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
-from flask_migrate import Migrate
+# from flask_migrate import Migrate
 # from flask_marshmallow import Marshmallow
 from flask_cors import CORS 
 from flask_jwt_extended import JWTManager
@@ -14,7 +14,7 @@ db = MongoEngine(app)
 # db = SQLAlchemy(app)
 # ma = Marshmallow(app)
 api = Api(app)
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
 from resources import User, UserLogin
