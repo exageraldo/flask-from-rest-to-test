9636c7eb84e954ce3a0719b5351ba551b38f0f8d
Geraldo Castro <victormatheuscastro@gmail.com>
2019-06-01 09:14:18 -0300

    Remove useless lines

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
-# from flask_migrate import Migrate
-# from flask_marshmallow import Marshmallow
 from flask_cors import CORS 
 from flask_jwt_extended import JWTManager
 from flask_mongoengine import MongoEngine
@@ -10,13 +7,9 @@ from config import configure_app
 
 app = Flask(__name__)
 CORS(app)
-#app.config.from_pyfile('config.py')
 configure_app(app)
 db = MongoEngine(app)
-# db = SQLAlchemy(app)
-# ma = Marshmallow(app)
 api = Api(app)
-# migrate = Migrate(app, db)
 jwt = JWTManager(app)
 
 from resources import User, UserLogin, Users
